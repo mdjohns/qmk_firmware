37a90ead62ef88dcb4c3e2925bcf3474ec709b74
Dylan Johnson <mdjohnson2@ualr.edu>
2021-04-15 13:51:37 -0500

    copy miryoku files to personal keymap folder

---
+++ keyboards/lily58/keymaps/mdjohns/rules.mk
@@ -0,0 +1,38 @@
+# copied from users/manna-harbour_miryoku/rules.mk
+# generated from users/manna-harbour_miryoku/miryoku.org  -*- buffer-read-only: t -*-
+
+MOUSEKEY_ENABLE = yes # Mouse keys
+EXTRAKEY_ENABLE = yes # Audio control and System control
+AUTO_SHIFT_ENABLE = yes # Auto Shift
+RGBLIGHT_ENABLE = yes
+ENCODER_ENABLE = yes
+
+# select alternative base layer alphas
+ifneq ($(strip $(MIRYOKU_ALPHAS)),)
+  OPT_DEFS += -DMIRYOKU_ALPHAS_$(MIRYOKU_ALPHAS)
+endif
+
+# select alternative nav
+ifneq ($(strip $(MIRYOKU_NAV)),)
+  OPT_DEFS += -DMIRYOKU_NAV_$(MIRYOKU_NAV)
+endif
+
+# select alternative subset mappings
+ifneq ($(strip $(MIRYOKU_MAPPING)),)
+  OPT_DEFS += -DMIRYOKU_MAPPING_$(MIRYOKU_MAPPING)
+endif
+
+# select alternative clipboard
+ifneq ($(strip $(MIRYOKU_CLIPBOARD)),)
+  OPT_DEFS += -DMIRYOKU_CLIPBOARD_$(MIRYOKU_CLIPBOARD)
+endif
+
+
+# If you want to change the display of OLED, you need to change here
+SRC +=  ./lib/rgb_state_reader.c \
+        ./lib/layer_state_reader.c \
+        ./lib/logo_reader.c \
+        ./lib/keylogger.c \
+        # ./lib/mode_icon_reader.c \
+        # ./lib/host_led_state_reader.c \
+        # ./lib/timelogger.c \
